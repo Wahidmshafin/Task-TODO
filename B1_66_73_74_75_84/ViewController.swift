d9b36b7f2962d49b04b4eddf80d0fc711e0af6ff
kuet <kuet-cse@kuets-iMac-5.local>
2022-11-03 15:42:46 +0600

    initial commit login page

---
+++ B1_66_73_74_75_84/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  B1_66_73_74_75_84
+//
+//  Created by kuet on 3/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var labelRegisterNow: UILabel!
+    @IBOutlet weak var labelNomember: UILabel!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBAction func buttonLogin(_ sender: Any) {
+    }
+    @IBAction func passwordTextfield(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var Heading: UILabel!
+    @IBAction func emailTextfield(_ sender: Any) {
+    }
+}
+
